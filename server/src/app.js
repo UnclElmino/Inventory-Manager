cb2406cdf990765f818c60c214ba2f2d1f4be352
UnclElmino <msiemilio@myseneca.ca>
2025-10-20 12:04:43 -0400

    corrected cors

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || '*', credentials: true }));
 app.use(express.json());
-
-const cors = require('cors');
 app.use(cors({
   origin: process.env.CLIENT_URL || 'https://inventory-manager-1-of6b.onrender.com',
   credentials: true
